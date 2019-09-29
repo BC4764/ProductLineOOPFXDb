bbc5d26b2048e68cd9da1bb8d570107fb24ec049
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-09-28 20:16:53 -0400

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,42 @@
+// benjamin cano
+//Date: 9/16/2019
+// main file of project that pulls most info
+
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.scene.control.ComboBox;
 
-public class Main extends Application {
+class Main { private ComboBox<String> cboTableName = new ComboBox<>();
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+  public static void main(String[] args) {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/HR";
+
+    final String USER = "";      //  Database credentials
+    final String PASS = "";
+    Connection conn;
+    Statement stmt;
+    try {
+        Class.forName(JDBC_DRIVER);    // STEP 1: Register JDBC driver
+        conn = DriverManager.getConnection(DB_URL, USER, PASS); //STEP 2: Open a connection
+        stmt = conn.createStatement();       //STEP 3: Execute a query
+
+        String sql = "SELECT * FROM PRODUCT";
+        stmt.executeUpdate(sql);
+        sql = "INSERT INTO PRODUCT (NAME, TYPE, MANUFACTURER) "
+            + "VALUES ('iPod','AUDIO','Apple')";
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
+        }
+    } catch (ClassNotFoundException | SQLException e) {         // STEP 4: Clean-up environment
+        e.printStackTrace();
+
     }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  }
 }
