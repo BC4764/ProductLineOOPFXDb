db097cd8e6aa0a8e50a267350231d62dbe9060bb
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-10-01 21:37:36 -0400

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,38 +4,23 @@
 
 package sample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import javafx.scene.control.ComboBox;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-class Main {
-  private ComboBox<String> cboTableName = new ComboBox<>();
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("SceneB.fxml"));
+    primaryStage.setTitle("Production Line Tracker");
+    primaryStage.setScene(new Scene(root, 640, 405));
+    primaryStage.show();
+  }
 
   public static void main(String[] args) {
-    final String JDBC_DRIVER = "org.h2.Driver";
-    final String DB_URL = "jdbc:h2:./res/HR";
-
-    final String USER = ""; //  Database credentials
-    final String PASS = "";
-    Connection conn;
-    Statement stmt;
-    try {
-      Class.forName(JDBC_DRIVER); // STEP 1: Register JDBC driver
-      conn = DriverManager.getConnection(DB_URL, USER, PASS); // STEP 2: Open a connection
-      stmt = conn.createStatement(); // STEP 3: Execute a query
-
-      String sql = "SELECT * FROM PRODUCT";
-      stmt.executeUpdate(sql);
-      sql = "INSERT INTO PRODUCT (NAME, TYPE, MANUFACTURER) " + "VALUES ('iPod','AUDIO','Apple')";
-      ResultSet rs = stmt.executeQuery(sql);
-      while (rs.next()) {
-        System.out.println(rs.getString(1));
-      }
-    } catch (ClassNotFoundException | SQLException e) { // STEP 4: Clean-up environment
-      e.printStackTrace();
-    }
+    launch(args);
   }
 }
