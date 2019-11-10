c990ebcbdfab606684b3ed17ddf9dcdaa3eb9981
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-11-09 19:08:27 -0500

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -21,6 +21,18 @@ public class Main extends Application {
   }
 
   public static void main(String[] args) {
+
+    Product productProduced = new Widget("iPod", "Apple", ItemType.AUDIO);
+    // test constructor used when creating production records from user interface
+    int numProduced = 3; // this will come from the combobox in the UI
+    int itemCount = 0;
+
+    for (int productionRunProduct = 0; productionRunProduct < numProduced; productionRunProduct++) {
+      ProductionRecord pr = new ProductionRecord(productProduced, itemCount++);
+      // using the iterator as the product id for testing
+      System.out.println(pr.toString());
+    }
     launch(args);
   }
 }
+
