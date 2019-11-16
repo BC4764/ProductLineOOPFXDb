7da26e1ae8e6bc08f5e6f63a7c472c43a95723cc
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-11-15 22:50:57 -0500

    Delete Widget.java

--- Widget.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import java.util.ArrayList;
-class Widget extends Product {
-  public Widget(String name, String manufacturer, ItemType type){
-    super(name, manufacturer, type);
-
-  }
-}
