64fe37e179ea7044d8b97cc676873d7e27914d83
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-11-26 21:06:06 -0500

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,18 +7,17 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * A program where users will input data about the products that they make into a database.
  *
  * @author Benjamin Cano
  */
-public class Main extends Application { // inspection code warning: Entry point.
+public class Main extends Application { 
 
   /**
-   * @param primaryStage the first thing the user sees
+   * @param primaryStage 
    * @throws Exception for the stage
    */
   @Override
-  public void start(Stage primaryStage) throws Exception { // inspection code warning: Entry point.
+  public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
     Scene scene = new Scene(root, 600, 425);
     primaryStage.setTitle("Product Tracker");
@@ -31,7 +30,7 @@ public class Main extends Application { // inspection code warning: Entry point.
    *
    * @param args - a String.
    */
-  public static void main(String[] args) { // inspection code warning: Entry point.
+  public static void main(String[] args) { 
     launch(args);
   }
 }
